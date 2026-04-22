e352c50e1a519850f689dd81b1261a7e4fde4482
ftz*! <68380939+feentzs@users.noreply.github.com>
2026-04-22 15:21:11 -0300

    colocar foto de perfil

--- lib/controllers/user_provider.dart
+++ lib/controllers/user_provider.dart
@@ -1,6 +1,8 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:nhac/models/user_model.dart';
 
 class UserProvider with ChangeNotifier {
@@ -16,12 +18,31 @@ class UserProvider with ChangeNotifier {
   if (user != null) {
     DocumentSnapshot doc = await _firestore.collection('usuarios').doc(user.uid).get();
     
-    if (doc.exists) {
-      _usuario = UserModel.fromMap(doc.data() as Map<String, dynamic>, doc.id);
-      notifyListeners();
+      if (doc.exists) {
+        _usuario = UserModel.fromMap(doc.data() as Map<String, dynamic>, doc.id);
+        notifyListeners();
+      }
     }
   }
-}
+
+  Future<void> atualizarFotoPerfil(File imagem) async {
+    final user = _auth.currentUser;
+    if (user == null) return;
+
+    final ref = FirebaseStorage.instance
+        .ref()
+        .child('perfil_fotos')
+        .child('${user.uid}.jpg');
+
+    await ref.putFile(imagem);
+    final url = await ref.getDownloadURL();
+
+    await _firestore.collection('usuarios').doc(user.uid).update({
+      'foto_url': url,
+    });
+
+    await carregarDadosUsuario();
+  }
 
   void limparUsuario() {
     _usuario = null;
